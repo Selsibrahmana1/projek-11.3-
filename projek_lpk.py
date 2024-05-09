a3007c8e8abc733f1e3b81f5f33041d7389a5d9c
Web lpk <165451878+Selsibrahmana1@users.noreply.github.com>
2024-05-09 13:01:23 +0700

    Update projek_lpk.py

--- projek_lpk.py
+++ projek_lpk.py
@@ -92,12 +92,15 @@ def calculate_density_section():
     volume = st.number_input('Masukkan volume larutan (mL):', min_value=0.01, step=0.01, value=st.session_state.data_input['volume'])
     st.session_state.data_input['volume'] = volume
 
-    # List untuk menyimpan nilai konsentrasi, volume, dan kerapatan
-    x_data = []  # Konsentrasi
-    y_data = []  # Kerapatan
-
     # Input data konsentrasi, volume, dan bobot
     st.write("Masukkan data konsentrasi dan bobot labu takar untuk perhitungan kerapatan:")
+    
+    data_input_table = {
+        'Konsentrasi (g/mL)': [],
+        'Bobot Labu Takar Isi (gram)': [],
+        'Bobot Labu Takar Kosong (gram)': []
+    }
+    
     for i in range(num_data):
         if i >= len(st.session_state.data_input['konsentrasi']):
             st.session_state.data_input['konsentrasi'].append(0)
@@ -113,53 +116,17 @@ def calculate_density_section():
         bobot_empty = st.number_input(f'Masukkan nilai rerata bobot labu takar kosong (gram) {i+1}:', format="%.4f", value=float(st.session_state.data_input['bobot_empty'][i]))
         st.session_state.data_input['bobot_empty'][i] = bobot_empty
         
-        # Menghitung bobot sebenarnya
-        weight = bobot_filled - bobot_empty
+        data_input_table['Konsentrasi (g/mL)'].append(konsentrasi)
+        data_input_table['Bobot Labu Takar Isi (gram)'].append(bobot_filled)
+        data_input_table['Bobot Labu Takar Kosong (gram)'].append(bobot_empty)
 
-        # Menghitung kerapatan
-        density = calculate_density(weight, volume)
-        if density is not None:
-            x_data.append(konsentrasi)
-            y_data.append(density)
+    # Tampilkan data input dalam bentuk tabel
+    st.table(data_input_table)
 
     # Tombol untuk menghitung hasil
     if st.button('Hitung'):
-        # Tampilkan hasil perhitungan kerapatan untuk setiap konsentrasi
-        st.header("Hasil Perhitungan Kerapatan untuk Setiap Konsentrasi", divider="violet")
-        for konsentrasi, density in zip(x_data, y_data):
-            st.write(f'Konsentrasi: {konsentrasi:.2f}, Kerapatan: {density:.4f} g/mL')
+        # List untuk menyimpan nilai konsentrasi, volume, dan kerapatan
+        x_data = []  # Konsentrasi
+        y_data = []  # Kerapatan
 
-        # Hitung persamaan regresi
-        slope, intercept, r = calculate_regression(x_data, y_data)
-
-        # Tampilkan hasil persamaan regresi
-        st.header("Persamaan Regresi", divider="violet")
-        st.write(f'Persamaan Regresi: y = {slope:.4f}x + {intercept:.4f}')
-        st.write(f'Nilai Regresi: {r:.4f}')
-        st.write(f'Slope (b): {slope:.4f}')
-        st.write(f'Intercept (a): {intercept:.4f}')
-
-        # Simpan hasil perhitungan ke variabel session_state
-        st.session_state.results = {
-            'x_data': x_data,
-            'y_data': y_data,
-            'slope': slope,
-            'intercept': intercept,
-            'r': r
-        }
-
-def about_us_section():
-    st.header("Kalkulator Hubungan Kerapatan dan Kepekatan Larutan Garam 🧪⚗", divider="rainbow")
-    st.write("""
-    Ini adalah kalkulator sederhana yang dikembangkan oleh Tim LPK. Terinspirasi dari praktik analisis fisika pangan mengenai praktikum 
-    dengan judul hubungan kerapatan dan kepekatan larutan garam. Dengan ini diharapkan dapat memudahkan untuk menghitung kerapatan 
-    dan kepekatan garam dalam larutan secara cepat dan tepat. Web Aplikasi disusun oleh :
-    1. Dinda Ariyantika              (2302520)
-    2. Ibnu Mustofa Giam             (2320529)
-    3. Putri Nabila Aji Kusuma       (2320546)
-    4. Salima Keisha Arthidia        (2320552)
-    5. Selsi Mei Doanna br Brahmana  (2320554)
-    """)
-
-if __name__ == "__main__":
-    main()
+        for konsentrasi, bobot
