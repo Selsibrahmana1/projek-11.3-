4781e8df37a19528d45651eb0e9e7bb5e75c2886
Web lpk <165451878+Selsibrahmana1@users.noreply.github.com>
2024-05-12 20:52:58 +0700

    Update projek_lpk.py

--- projek_lpk.py
+++ projek_lpk.py
@@ -28,7 +28,7 @@ def calculate_density_section():
             color: #ffffff; /* Warna putih */
         }
         .stMarkdown h1 {
-            color: silver !A8A9AD; /* Warna silver */
+            color: #A8A9AD !important; /* Warna silver */
             padding-bottom: 10px; /* Spasi antara judul dan konten */
         }
         .stMarkdown {
